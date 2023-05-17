9435ffd5bdc803f7326d19b3ad4da9574a1d3b0e
TK081 <tiseagan2001@gmail.com>
2023-05-17 14:21:19 -0400

    Added dummy data & drop down menu functionality

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import React, { useState } from "react";
 import './App.css';
 import image from './images/spcp.jpg';
 import { FaQuestion } from 'react-icons/fa';
-
+import excelFile1 from './excelfiles/Dummy_Data_1.xlsx';
+import excelFile2 from './excelfiles/Dummy_Data_2.xlsx';
+import excelFile3 from './excelfiles/Dummy_Data_3.xlsx';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
-
 function App() {
 
+  // Code for search bar logic
   const [searchTerm, setSearchTerm] = useState("");
 
   const handleSearch = () => {
     // Handle search logic here
   };
 
+  // Code for help button pop up message
   const [showPopup, setShowPopup] = useState(false);
   
   const handleHelp = () => {
@@ -22,11 +25,20 @@ function App() {
     setTimeout(() => {
       setShowPopup(false);
     }, 5000); // Hide the popup after 2 seconds
-
   };
 
-  // Inside the component
+  // Code for drop down menu for excel files
   const [selectedFile, setSelectedFile] = useState("");
+
+  const options = [
+    { value: "Dummy_Data_1.xlsx", label: "Profile 1" },
+    { value: "Dummy_Data_2.xlsx", label: "Profile 2" },
+    { value: "Dummy_Data_3.xlsx", label: "Profile 3" }
+  ];
+
+  const handleFileChange = (e) => {
+    setSelectedFile(e.target.value);
+  };
 
   return (
     <div className = "container">
@@ -34,6 +46,16 @@ function App() {
       <p>Basic information about the webpage</p>  
       <h2>Drop down Menu</h2>
       <p>Drop down menu paragraph</p>
+      <div>
+      <select value={selectedFile} onChange={(e) => setSelectedFile(e.target.value)}>
+      <option value="">Select Excel File</option>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+      </div>
       <img className="logo" src={image} alt="SPCP Logo" />
       <h2>Header about search bar</h2>
       <p>Placeholder information about webpage, database, and organization </p>
@@ -53,7 +75,6 @@ function App() {
       {showPopup && <div className="popup">Search in this format: "Indicator/Area"</div>}
       <button className="button">Download</button>
       
-
     </div>
   );
 }
