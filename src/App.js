df601e5d487a0657cd6940f6e80bb4c19173bb1c
TK081 <tiseagan2001@gmail.com>
2023-05-24 15:00:19 -0400

    Updated App.js with code formatting and got rid of unused or needless lines

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import React, { useState } from "react";
 import './App.css';
 import image from './images/spcp.jpg';
 import {FaQuestion , FaDownload} from 'react-icons/fa';
-import excelFile1 from './excelfiles/Cleaned_Up_Data.xlsx';
-import excelFile2 from './excelfiles/South_Asian_Cleaned_Up_Data_-_Provinces.xlsx';
 import SearchBar from "./SearchBar";
 import 'typeface-montserrat';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
 
@@ -33,45 +29,49 @@ function App() {
   return (
 
     <div className = "container">
+
       <h1>Portraits of Peel Database Webpage</h1>
+
       <div className = "paragraph">
-      <p>Are you looking for information about South Asians within Peel, provincially, or nationally? </p>  
-      <p>Look no further! Portraits of Peel has been collecting data since 1965 and helping curious </p> 
-      <p>minds like yours use our data analysis to your advantage! Click the drop-down menu and </p>
-      <p>get the information you need :)</p>
+        <p>Are you looking for information about South Asians within Peel, provincially, or nationally? </p>  
+        <p>Look no further! Portraits of Peel has been collecting data since 1965 and helping curious </p> 
+        <p>minds like yours use our data analysis to your advantage! Click the drop-down menu and </p>
+        <p>get the information you need :)</p>
       </div>
 
       <div className="paragraphtwo">
-      <h2>Dropdown Menu</h2>
-      <p>This dropdown menu will allow you to cycle through three different profiles, which</p>
-      <p>all contain unique data for specific areas.</p>
+        <h2>Dropdown Menu</h2>
+        <p>This dropdown menu will allow you to cycle through three different profiles, which</p>
+        <p>all contain unique data for specific areas.</p>
       </div>
+
       <div >
-      <select className="dropdown" value={selectedSheet} onChange={(event) => setSelectedSheet(event.target.value)}>
-      <option value="">Select Excel File</option>
-      {options.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.label}
-        </option>
-      ))}
-    </select>
-
+        <select className="dropdown" value={selectedSheet} onChange={(event) => setSelectedSheet(event.target.value)}>
+        <option value="">Select Excel File</option>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+        </select>
       </div>
+
       <img className="logo" src={image} alt="SPCP Logo" />
+
       <div className="paragraphthree">
-      <h2>Searchbar for database and search queries </h2>
-      <p>This search bar allows for the user to input unique searches, which will allow</p>
-      <p> for the needed table of data to be outputted.</p>
+        <h2>Searchbar for database and search queries </h2>
+        <p>This search bar allows for the user to input unique searches, which will allow</p>
+        <p> for the needed table of data to be outputted.</p>
       </div>
-      <SearchBar/>
+
+        <SearchBar/>
+
       <div>
-
-      <button className="button" onClick={handleHelp}><FaQuestion/></button>
-      {showPopup && <div className="popup">Search in this format: "Indicator/Area"</div>}
-      <button className="button"><FaDownload/></button>
-
+        <button className="button" onClick={handleHelp}><FaQuestion/></button>
+        {showPopup && <div className="popup">Search in this format: "Indicator/Area"</div>}
+        <button className="button"><FaDownload/></button>
       </div>
-  
+
     </div>
   );
 }
