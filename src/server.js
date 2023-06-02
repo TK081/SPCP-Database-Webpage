2159278f7f8ca896cec554cd4f5785fe7ed12941
TK081 <tiseagan2001@gmail.com>
2023-06-02 15:39:13 -0400

    Added first portion of search in excel sheet in server.js

--- src/server.js
+++ src/server.js
@@ -15,7 +15,6 @@ app.use(express.static('public'));
 *-----------------------*
 */
 
-
 // Enable CORS for all routes
 app.use(cors());
 
@@ -24,52 +23,82 @@ const path = require('path');
 const XLSX = require('xlsx');
 const filePath = path.join(__dirname, 'excelfiles', 'praythisworksv2.xlsx');
 
-app.get('/api/sheetNames', (req, res) => {
-  const workbook = XLSX.readFile(filePath);
-  const sheetNames = workbook.SheetNames;
-  res.json({ sheetNames });
-});
+// Load the workbook
+const workbook = XLSX.readFile(filePath);
+const sheetName = 'Peel Region Statistics'; // Replace with the actual sheet name
+const sheet = workbook.Sheets[sheetName];
+const columnLetter = 'A';
+const boldedCells = [];
+
+// for (let row = 1; worksheet[`${columnLetter}${row}`]; row++) {
+//   const cell = worksheet[`${columnLetter}${row}`];
+//   // console.log(cell);
+
+//   if (cell.s && cell.s.bold) {
+//     boldedCells.push(cell.v); // Append the cell value to the array
+//     // console.log(cell.s);
+//     // console.log(cell.s.bold);
+//   }
+// }
+for (let cellAddress in sheet) {
+  if (sheet.hasOwnProperty(cellAddress)) {
+    // Get the cell value and style
+    const cell = sheet[cellAddress];
+    const value = cell.v;
+    const style = cell.s;
+    // console.log(cell);
+    // console.log(value);
+    // console.log(style);
+
+    // Check if the cell is in the first column and has bold formatting
+    if (cellAddress.match(/^  A\d+$/) && style && style.font && style.font.bold) {
+      // Add the cell value to the boldedCells array
+      // console.log(value);
+      // console.log(style);
+      boldedCells.push(value);
+    }
+  }
+}
+
+console.log(boldedCells);
+
+// app.get('/api/sheetNames', (req, res) => {
+//   const workbook = XLSX.readFile(filePath);
+//   const sheetNames = workbook.SheetNames;
+//   res.json({ sheetNames });
+// });
 
 // Check endpoint in both server.js & Dropdown.js
 // http://localhost:8000/api/sheet/Canada%20Statistics
 // http://localhost:8000/api/sheet/Peel%20Region%20Statistics
-app.get('/api/sheet/:sheetName',(req, res) => {
+// app.get('/api/sheet/:sheetName',(req, res) => {
 
-  const workbook = XLSX.readFile(filePath);
-  const sheetName = decodeURIComponent(req.params.sheetName);
-  const sheet = workbook.Sheets[sheetName];
-  console.log('Sheet Object:', sheet);
-  const jsonData = XLSX.utils.sheet_to_json(sheet);
-  res.json({ sheetData: jsonData });
+//   const workbook = XLSX.readFile(filePath);
+//   const sheetName = decodeURIComponent(req.params.sheetName);
+//   const sheet = workbook.Sheets[sheetName];
+//   console.log('Sheet Object:', sheet);
+//   const jsonData = XLSX.utils.sheet_to_json(sheet);
+//   res.json({ sheetData: jsonData });
 
-  // const sheetName = workbook.SheetNames;
-  // res.json({sheetName});
-  //const worksheet = workbook.Sheets[sheetnames];
-  //const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheetnames]);
+//   // const sheetName = workbook.SheetNames;
+//   // res.json({sheetName});
+//   //const worksheet = workbook.Sheets[sheetnames];
+//   //const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheetnames]);
 
-});
+// });
 
 // http://localhost:8000/api/search?query=helloworld
+// app.get('/api/search', (req, res) => {
 
-app.get('/api/search', (req, res) => {
+//   //res.send('Hello from the search endpoint');
+//   console.log('Received request at /api/search');
+//   const query = req.query.query;
+//   // Implement our own search logic 
+//   console.log('Query:',query);
+//   res.json(query)
 
-  //res.send('Hello from the search endpoint');
-  console.log('Received request at /api/search');
-  const query = req.query.query;
-  // Implement our own search logic 
-  console.log('Query:',query);
-  res.json(query)
-
-});
+// });
 
 app.listen(port, () => {
   console.log('Server is running on port 8000');
 });
-
-// app.post('/search', (req, res) => {
-//     const { searchQuery } = req.body;
-//     // Perform any necessary actions with the search query
-//     console.log(searchQuery);
-//     // Send a response back to the client
-//     res.send('Search results');
-//   });
