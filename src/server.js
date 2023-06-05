694fe4345a1a3b41bc2203fc44dee1ad8306cb3e
TK081 <tiseagan2001@gmail.com>
2023-06-05 16:35:29 -0400

    Reimplemented get request for sheet names in server.js

--- src/server.js
+++ src/server.js
@@ -72,11 +72,26 @@ const readExcel = async () => {
 
 // readExcel();
 
+// SheetJS Get Request for sheetNames
 // app.get('/api/sheetNames', (req, res) => {
 //   const workbook = XLSX.readFile(filePath);
 //   const sheetNames = workbook.SheetNames;
 //   res.json({ sheetNames });
 // });
+
+// ExcelJS Get Request for sheetNames
+// app.get('/api/sheetNames', (req, res) => {
+//   const workbook = new ExcelJS.Workbook();
+//   workbook.xlsx.readFile(filePath)
+//     .then(() => {
+//       const sheetNames = workbook.sheetNames;
+//       res.json({ sheetNames });
+//     })
+//     .catch((error) => {
+//       res.status(500).json({ error: 'Failed to read the file' });
+//     });
+// });
+
 
 // Check endpoint in both server.js & Dropdown.js
 // http://localhost:8000/api/sheet/Canada%20Statistics
@@ -98,16 +113,16 @@ const readExcel = async () => {
 // });
 
 // http://localhost:8000/api/search?query=helloworld
-// app.get('/api/search', (req, res) => {
+app.get('/api/search', (req, res) => {
 
-//   //res.send('Hello from the search endpoint');
-//   console.log('Received request at /api/search');
-//   const query = req.query.query;
-//   // Implement our own search logic 
-//   console.log('Query:',query);
-//   res.json(query)
+  //res.send('Hello from the search endpoint');
+  console.log('Received request at /api/search');
+  const query = req.query.query;
+  // Implement our own search logic 
+  console.log('Query:',query);
+  res.json(query)
 
-// });
+});
 
 app.listen(port, () => {
   console.log('Server is running on port 8000');
