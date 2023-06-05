4296b160e4a36a02c32723789d03de57bc539c7e
TK081 <tiseagan2001@gmail.com>
2023-06-05 16:29:09 -0400

    Implemented basis for search logic in server.js

--- src/server.js
+++ src/server.js
@@ -20,36 +20,92 @@ app.use(cors());
 
 // Code to read in excel data file using XLSX & SheetJS
 const path = require('path');
-const XLSX = require('xlsx');
+// const XLSX = require('xlsx');
+const ExcelJS = require('exceljs');
 const filePath = path.join(__dirname, 'excelfiles', 'praythisworksv2.xlsx');
 
 // Load the workbook
-const workbook = XLSX.readFile(filePath, {cellStyles:true});
-const sheetName = 'Peel Region Statistics'; // Replace with the actual sheet name
-const worksheet = workbook.Sheets[sheetName];
-const columnLetter = 'A';
-const boldedCells = [];
+// const workbook = await ExcelJS.readFile(filePath, {cellStyles:true});
+// const sheetName = 'Peel Region Statistics'; // Replace with the actual sheet name
+// const worksheet = workbook.Sheets[sheetName];
+// const columnLetter = 'A';
+// const boldedCells = [];
+
+// const workbook = new ExcelJS.Workbook();
+// const workbook = await workbook.xlsx.readFile(filePath);
+// const sheetNames = workbook.SheetNames;
+// const worksheet = workbook.getWorksheet(sheetNames); // Replace 'Sheet1' with the actual sheet name
+
+// const ExcelJS = require('exceljs');
+
+const readExcel = async () => {
+  // Code to loop through the first column and extract all bolded indicators
+  // const workbook = new ExcelJS.Workbook();
+  // await workbook.xlsx.readFile(filePath);
+  // const worksheet = workbook.getWorksheet('Canada Statistics'); // Replace 'Sheet1' with the name of your sheet
+
+  // const boldedText = [];
+
+  // worksheet.getColumn('A').eachCell((cell, rowNumber) => {
+  //   if (cell.font && cell.font.bold) {
+  //     boldedText.push(cell.value);
+  //   }
+  // });
+
+  // console.log(boldedText);
+
+  const readExcel = async () => {
+    const workbook = new ExcelJS.Workbook();
+    await workbook.xlsx.readFile(filePath);
+    const worksheet = workbook.getWorksheet('Canada Statistics'); // Replace 'Sheet1' with the name of your sheet
+  
+    const boldedText = [];
+  
+    const startRow = 1; // Start from row 2 (excluding header)
+    const endRow = 225; // End at row 10
+  
+    for (let rowNumber = startRow; rowNumber <= endRow; rowNumber++) {
+      const cell = worksheet.getCell(`A${rowNumber}`); // Assuming you're searching in the first column
+      if (cell.font && cell.font.bold) {
+        boldedText.push({
+          text: cell.value,
+          position: {
+            row: rowNumber,
+            column: 1, // Assuming you're searching in the first column
+          },
+        });
+      }
+    }
+  
+    console.log(boldedText);
+  };
+  
+  readExcel();
+
+};
+
+// readExcel();
 
 // function PrintAndReturn(value, label){
 //   console.log(`${label}: ${value}`);
 //   return value;
 // }
 
-for (let row = 1; worksheet[`${columnLetter}${row}`] || worksheet[`${columnLetter}${row + 1}`]; row++) {
-  const cell = worksheet[`${columnLetter}${row}`];
-  console.log(cell);
-  if (!cell){
-    continue;
-  }
+// for (let row = 1; worksheet[`${columnLetter}${row}`] || worksheet[`${columnLetter}${row + 1}`]; row++) {
+//   const cell = worksheet[`${columnLetter}${row}`];
+//   console.log(cell);
+//   if (!cell){
+//     continue;
+//   }
 
-  if (cell.s && cell.s.bold) {
-    boldedCells.push(cell.v); // Append the cell value to the array
-    // console.log(cell.s);
-    // console.log(cell.s.bold);
-  }
-}
+//   if (cell.s && cell.s.bold) {
+//     boldedCells.push(cell.v); // Append the cell value to the array
+//     // console.log(cell.s);
+//     // console.log(cell.s.bold);
+//   }
+// }
 
-console.log(boldedCells);
+// console.log(boldedCells);
 
 // for (let cellAddress in sheet) {
 //   if (sheet.hasOwnProperty(cellAddress)) {
