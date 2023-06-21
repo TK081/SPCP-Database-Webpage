7d396070024dbb8f61248a98dc22e65efc8fbe91
Helix2002 <ronakray2002@gmail.com>
2023-06-21 15:43:53 -0400

    Graphing data works

--- src/SearchOutput.js
+++ src/SearchOutput.js
@@ -1,29 +1,27 @@
-import React, { useEffect,useState , useRef } from 'react';
+import React, { useEffect,useState } from 'react';
 import axios from 'axios';
 import { render } from '@testing-library/react';
-import {BarElement, CategoryScale, Chart, LinearScale} from 'chart.js';
-import { Bar } from 'react-chartjs-2'
+import { Bar, Pie } from 'react-chartjs-2';
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
 
 const SearchOutput = ({query, sheet}) => {
 
     const [data, setData] = useState([]);
-    const chartRef = useRef(null);
+    // const chartRef = useRef(null);
 
-    Chart.register(
-      CategoryScale,
-      LinearScale,
-      BarElement
-    )
+    // Chart.register(
+    //   CategoryScale,
+    //   LinearScale,
+    //   BarElement
+    // )
     
     useEffect(() => {
         console.log(query);
         if (query.length === 0 || sheet.length === 0) {
             return;
         } 
-
-        if (chartRef.current !== null) {
-          chartRef.current.destroy();
-        }
 
         axios.get('http://localhost:8000/api/search', {params: {query, sheet}})
         .then((response) => response.data)
@@ -65,54 +63,125 @@ const SearchOutput = ({query, sheet}) => {
         );
       };
 
-      const renderBarChart = () => {
-        if (Object.keys(data).length === 0) {
+      const renderChart = () => {
+        if (Object.keys(data).length === 0 || !areasValues || areasValues.length === 0) {
           return null;
         }
-    
+
+        const isPercentage = areasValues.every((value) => {
+          if (typeof value !== 'string') {
+            return false; // Not a string, not a percentage
+          }
+          return value.endsWith('%');
+        });
+
         const chartData = {
           labels: categories,
-          datasets: [{
-            label: `${indicator}/${area}`,
-            data: areasValues,
-            borderWidth: 1
-          }]
+          datasets: [
+            {
+              label: `${indicator}/${area}`,
+              data: areasValues.map((value) => {
+                if (typeof value === 'string') {
+                  return parseFloat(value.replace('%', ''));
+                }
+                return value;
+              }),
+              backgroundColor: generateColors(categories.length),
+              borderWidth: 1,
+            },
+          ],
         };
-    
-        const options = {
-          maintainAspectRatio: false,
-          plugins: {
-            title: {
+      
+
+        if (isPercentage) {
+      
+          const options = {
+            maintainAspectRatio: true,
+            plugins: {
+              title: {
                 display: true,
-                text:`${indicator}/${area}`,
+                text: `${indicator}/${area}`,
                 fontSize: 16,
                 padding: {
                   top: 10,
-                  bottom: 10
-                }
-            }
-          },
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          },
-          legend: {
-            labels: {
-              fontSize: 26
-            }
-          }
-        };
+                  bottom: 10,
+                },
+              },
+            },
+            responsive: true,
+          };
+      
+          return (
+            <div>
+              <div style={{ maxWidth: '500px', margin: '0 auto' }}>
+                <Pie data={chartData} options={options} />
+              </div>
+            </div>
+          );
+
+
+        }
+
+        else{
+      
+          const options = {
+            maintainAspectRatio: true,
+            plugins: {
+              title: {
+                display: true,
+                text: `${indicator}/${area}`,
+                fontSize: 16,
+                padding: {
+                  top: 10,
+                  bottom: 10,
+                },
+              },
+            },
+            scales: {
+              x: {
+                display: false,  // Set display to false to hide the x-axis labels
+              },
+              y: {
+                beginAtZero: true,
+              },
+            },
+            responsive: true,
+            interaction: {
+              mode: 'index',
+              intersect: false,
+            },
+          };
+  
+          const chartContainerStyle = {
+            width: '100%',
+            minWidth: '950px',
+            maxWidth: '1500px', // Adjust the maximum width based on your preference
+            margin: '0 auto', // Center the chart horizontally
+          };
+  
+          return (
+            <div style={chartContainerStyle}>
+              <Bar data={chartData} options={options} />
+            </div>
+          );
+
+        }
     
-        return (
-          <div>
-            <Bar 
-              data={chartData}
-              height={200}
-              options={options}
-            />
-          </div>
-        );
+       
+      };
+
+      const generateColors = (numColors) => {
+        const colors = [];
+        for (let i = 0; i < numColors; i++) {
+          const color = `rgba(${getRandomValue(0, 255)}, ${getRandomValue(0, 255)}, ${getRandomValue(0, 255)}, 0.6)`;
+          colors.push(color);
+        }
+        return colors;
+      };
+      
+      // Function to generate a random number between min and max (inclusive)
+      const getRandomValue = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min);
       };
 
       return (
@@ -122,9 +191,9 @@ const SearchOutput = ({query, sheet}) => {
           {renderTable()}
         </div>
 
-        <div class="graph">
-          {renderBarChart()}
-        </div>
+        <div className="graph">
+          {renderChart()}
+          </div>
           
       </div>
 
