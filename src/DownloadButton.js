a49beb2205d068b786374610c14deed34103103b
TK081 <tiseagan2001@gmail.com>
2023-06-22 17:49:12 -0400

    made a minor fix to the screenshot functionality

--- src/DownloadButton.js
+++ src/DownloadButton.js
@@ -1,30 +1,28 @@
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 import {FaDownload} from 'react-icons/fa';
-import SearchOutput from './SearchOutput';
 
 const DownloadButton = () => {
     const handleCaptureScreenshot = () => {
-    // Get the target HTML element to capture (e.g., a specific div with a ref)
-    const targetElement = document.getElementById('captureElement');
+      // Get the target HTML element to capture (e.g., a specific div with a ref)
+      const targetElement = document.getElementById('render');
+          // Use html2canvas to capture the screenshot of the target element
+          html2canvas(targetElement).then((canvas) => {
+            // Create a new jsPDF instance
+            const pdf = new jsPDF();
 
-        // Use html2canvas to capture the screenshot of the target element
-        html2canvas(targetElement).then((canvas) => {
-        // Create a new jsPDF instance
-        const pdf = new jsPDF();
+            // Calculate the aspect ratio to fit the screenshot in the PDF page
+            const imgWidth = 210; // Width of the PDF page (in mm)
+            const imgHeight = (canvas.height * imgWidth) / canvas.width; // Calculate the height based on the aspect ratio
 
-        // Calculate the aspect ratio to fit the screenshot in the PDF page
-        const imgWidth = 210; // Width of the PDF page (in mm)
-        const imgHeight = (canvas.height * imgWidth) / canvas.width; // Calculate the height based on the aspect ratio
+            // Convert the captured canvas to an image and add it to the PDF
+            const screenshotData = canvas.toDataURL('image/png');
+            pdf.addImage(screenshotData, 'PNG', 0, 0, imgWidth, imgHeight);
 
-        // Convert the captured canvas to an image and add it to the PDF
-        const screenshotData = canvas.toDataURL('image/png');
-        pdf.addImage(screenshotData, 'PNG', 0, 0, imgWidth, imgHeight);
+            // Save the PDF and trigger the download
+            pdf.save('dataoutput.pdf');
 
-        // Save the PDF and trigger the download
-        pdf.save('dataoutput.pdf');
         });
-
     };
 
     return (
@@ -32,6 +30,7 @@ const DownloadButton = () => {
           {/* Place the target element you want to capture */}
           <div>
             {/* Content you want to capture */}
+            <div id='render'></div>
           </div>
     
           {/* Button to trigger the screenshot and PDF generation */}
